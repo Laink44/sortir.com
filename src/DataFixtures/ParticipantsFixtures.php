066609b219fc5fb70da08d05a9ec564bada6b639
edescham2018 <eric.deschamps2018@campus-eni.fr>
2019-09-30 16:45:14 +0200

    Implement Fixture Participants

---
+++ src/DataFixtures/ParticipantsFixtures.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Participants;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class ParticipantsFixtures extends Fixture
+{
+    private $encoder;
+    public function __construct(UserPasswordEncoderInterface $encoder) {
+        $this->encoder = $encoder;
+    }
+
+    //Génère des participants fictifs
+    public function load(ObjectManager $manager)
+    {
+
+        $faker = \Faker\Factory::create('fr_FR');
+        for ($nbpart=1; $nbpart<=5; $nbpart ++) {
+            $participant = new Participants();
+            // ATTENTION : on s'assure de l'unicité
+            $participant -> setPseudo( strtolower( substr($faker->unique()->firstName,2).substr($faker->unique()->lastName,3)));
+            $participant -> setNom( $faker->lastName);
+            $participant -> setPrenom($faker ->firstName);
+            $participant -> setTelephone(($faker->unique()->phoneNumber));
+            $participant -> setMail(($faker->unique()->email));
+            $password = $this->encoder->encodePassword($participant,'pass_1234"');
+            $participant ->setMotDePasse($password);
+            $participant -> setActif($faker->boolean);
+            $participant -> setAdministrateur($faker->boolean);
+            $participant -> setSitesNoSite($faker->randomDigit);
+
+
+
+
+        }
+
+
+        // $product = new Product();
+        // $manager->persist($product);
+
+        $manager->flush();
+    }
+}
