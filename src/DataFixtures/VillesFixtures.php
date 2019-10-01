99ad2848456dc6413ec7b94194e75102884c2b9e
Ian Wheelbuckets <rousseauxyannarnaud@gmail.com>
2019-10-01 09:10:27 +0200

    Correction VillesFixtures

--- src/DataFixtures/VillesFixtures.php
+++ src/DataFixtures/VillesFixtures.php
@@ -11,15 +11,12 @@ class VillesFixtures extends Fixture
     public function load(ObjectManager $manager)
     {
         $faker = \Faker\Factory::create( 'fr_FR' );
-
         $json_source = file_get_contents( 'public/data/villes.json' );
         $json_data = json_decode( $json_source, true );
         foreach( $json_data as $city ){
             $ville = new Villes();
             $ville -> setNomVille( $city['nom'] );
             $ville -> setCodePostal( $city['code_postal'] );
-            $em = $this->getDoctrine()->getManager();
-            $em->persist( $ville );
             $manager->flush();
         }
     }
