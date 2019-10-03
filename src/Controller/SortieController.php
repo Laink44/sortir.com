112e502a19e74358245320295928310e9033eec4
Laink44 <eric.deschamps2018@campus-eni.fr>
2019-10-03 11:32:50 +0200

    fix sortie

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -26,6 +26,7 @@ class SortieController extends Controller
     public function index(EntityManagerInterface $em,
                              Request $request)
     {
+
         $sortie = new Sortie();
         $sortieForm = $this->createForm(CreateSortieType::class, $sortie);
 
@@ -49,7 +50,7 @@ class SortieController extends Controller
 
     /**
      * @Route(
-     * "/sortie",
+     * "/sortie_add",
      * name="create_sortie",
      * methods={"POST"}
      * )
