07c26e59eab3554c0215d6c766d13c468e80b609
qmartinez2018 <quentin.martinez@outlook.com>
2019-10-03 11:40:14 +0200

    minor fixes navigation

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -15,7 +15,7 @@ class SortieController extends Controller
 {
     /**
      * @Route(
-     * "/sortie",
+     * "/create_sortie",
      * name="index_sortie",
      * methods={"GET"}
      * )
@@ -50,7 +50,7 @@ class SortieController extends Controller
 
     /**
      * @Route(
-     * "/sortie_add",
+     * "/create_sortie",
      * name="create_sortie",
      * methods={"POST"}
      * )
