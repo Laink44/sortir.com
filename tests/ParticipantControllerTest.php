5b97c864b4302a95c71d03f3f53292044a3e427a
qmartinez2018 <quentin.martinez@outlook.com>
2019-10-03 10:50:39 +0200

    home controller

---
+++ tests/ParticipantControllerTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class ParticipantControllerTest extends WebTestCase
+{
+    public function testHomePage()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+
+        $this->assertSame(200, $client->getResponse()->getStatusCode());
+        $this->assertContains('Connexion', $crawler->filter('h1')->text());
+    }
+
+
+    private function logIn()
+    {
+        $session = $this->client->getContainer()->get('session');
+
+        $firewallName = 'main';
+        // if you don't define multiple connected firewalls, the context defaults to the firewall name
+        // See https://symfony.com/doc/current/reference/configuration/security.html#firewall-context
+        $firewallContext = 'secured_area';
+
+        // you may need to use a different token class depending on your application.
+        // for example, when using Guard authentication you must instantiate PostAuthenticationGuardToken
+        $token = new UsernamePasswordToken('admin', null, $firewallName, ['ROLE_ADMIN']);
+        $session->set('_security_'.$firewallContext, serialize($token));
+        $session->save();
+
+        $cookie = new Cookie($session->getName(), $session->getId());
+        $this->client->getCookieJar()->set($cookie);
+    }
+}
