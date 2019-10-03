022a5088cdf0a51a6c3f1840bb52860c02482a81
Ian Wheelbuckets <rousseauxyannarnaud@gmail.com>
2019-10-03 14:00:59 +0200

    Ajout paginateur

--- src/Controller/SiteController.php
+++ src/Controller/SiteController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Site;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SiteController extends Controller
@@ -15,10 +17,11 @@ class SiteController extends Controller
      * methods={"GET"}
      * )
      */
-    public function adminSite()
+    public function adminSite( PaginatorInterface $paginator, Request $request )
     {
+        $allSites = $this -> getRepo() -> findAllSites();
         return $this->render('admin/admin_site.html.twig', [
-            'allSites' => $this -> getAllSites()
+            'allSites' => $this -> getPaginatedList( $allSites, $paginator, $request )
         ]);
     }
 
@@ -29,15 +32,17 @@ class SiteController extends Controller
      * methods={"GET"}
      * )
      */
-    public function addSite( $nomSite = '' )
+    public function addSite( $nomSite = '', PaginatorInterface $paginator, Request $request )
     {
         $newSite = new Site();
         $newSite -> setNomSite( $nomSite );
         $this -> getEm() -> persist( $newSite );
         $this -> getEm() -> flush();
 
+        $allSites = $this -> getRepo() -> findAllSites();
+
         return $this->render('admin/admin_site_table.html.twig', [
-            'allSites' => $this -> getAllSites()
+            'allSites' => $this -> getPaginatedList( $allSites, $paginator, $request )
         ]);
     }
 
@@ -48,14 +53,16 @@ class SiteController extends Controller
      * methods={"GET"}
      * )
      */
-    public function removeSite( $id = '' )
+    public function removeSite( $id = '', PaginatorInterface $paginator, Request $request )
     {
         $siteToRemove = $this -> getRepo() -> find( $id );
         $this -> getEm() -> remove( $siteToRemove );
         $this -> getEm() -> flush();
 
+        $allSites = $this -> getRepo() -> findAllSites();
+
         return $this->render('admin/admin_site_table.html.twig', [
-            'allSites' => $this -> getAllSites()
+            'allSites' => $this -> getPaginatedList( $allSites, $paginator, $request )
         ]);
     }
 
@@ -66,15 +73,17 @@ class SiteController extends Controller
      * methods={"GET"}
      * )
      */
-    public function editSite( $id = '', $nomSite = '' )
+    public function editSite( $id = '', $nomSite = '', PaginatorInterface $paginator, Request $request )
     {
         $siteToEdit = $this -> getRepo() -> find( $id );
         $siteToEdit -> setNomSite( $nomSite );
         $this -> getEm() -> persist( $siteToEdit );
         $this -> getEm() -> flush();
 
+        $allSites = $this -> getRepo() -> findAllSites();
+
         return $this->render('admin/admin_site_table.html.twig', [
-            'allSites' => $this -> getAllSites()
+            'allSites' => $this -> getPaginatedList( $allSites, $paginator, $request )
         ]);
     }
 
@@ -85,23 +94,37 @@ class SiteController extends Controller
      * methods={"GET"}
      * )
      */
-    public function searchSite( $nomSite = '' )
+    public function searchSite( $nomSite = '', PaginatorInterface $paginator, Request $request )
     {
         $foundSites = $this -> getRepo() -> getBySiteName( $nomSite, 0, 5 );
 
         if( $nomSite === 'empty' )
         {
-            $foundSites = $this -> getAllSites();
+            $foundSites = $this -> getRepo() -> findAllSites();
         }
 
         return $this->render('admin/admin_site_table.html.twig', [
-            'allSites' => $foundSites
+            'allSites' => $this -> getPaginatedList( $foundSites, $paginator, $request )
         ]);
     }
 
     public function getAllSites()
     {
         return $this -> getRepo() -> findAll();
+    }
+
+    public function getPaginatedList( Array $listOfObjectsToPaginate, PaginatorInterface $paginator, Request $request )
+    {
+        $paginatedObjects = $paginator -> paginate(
+            $listOfObjectsToPaginate,
+            $request -> query -> getInt( 'page', 1 ),
+            5
+        );
+
+        $paginatedObjects -> setTemplate( 'pagination/twitter_bootstrap_v4_pagination.html.twig' );
+        $paginatedObjects -> setUsedRoute( 'gestion_site' );
+
+        return $paginatedObjects;
     }
 
     public function getEm() {
